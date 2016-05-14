9c6483652c34da2e8bd95fc9333ffc1e7811ca03
Romanos Skiadas <rom.skiad@gmail.com>
2016-05-15 00:03:44 +0300

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+# Class: packages
+# ===========================
+#
+# Installs packages
+#
+# Parameters
+# ----------
+#
+# Authors
+# -------
+#
+# Romanos Skiadas <rom.skiad@gmail.com>
+#
+# Copyright
+# ---------
+#
+# Copyright 2016 Romanos Skiadas, unless otherwise noted.
+#
+class packages {
+
+
+}
